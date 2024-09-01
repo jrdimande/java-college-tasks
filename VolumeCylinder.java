145a0f2b32bd797fa665798b7ea45ee480a307ba
Timóteo Dimande júnior <timoteojrdimande@gmail.com>
2024-09-01 17:40:22 +0000

    Add files via upload
    Este programa em java calcula o volume do cilíndro com base no raio e altura fornecidos pelo usuário

---
+++ VolumeCylinder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class VolumeCylinder{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Introduz o raio: ");
+        double radius = sc.nextDouble();
+
+        System.out.print("Introduz a Altura: ");
+        double height = sc.nextDouble();
+
+        double volume = 3.14 * radius * radius * height;
+
+        System.out.println("O volume do cilíndro é: "+volume);
+
+        
+    }
+}
