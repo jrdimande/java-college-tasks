502349605291b5bac035eb3b5691cd8db62a1739
Timóteo Dimande júnior <timoteojrdimande@gmail.com>
2024-09-01 16:05:31 +0000

    Add files via upload
    Este é um programa Java simples desenvolvido como exercício de faculdade para exibir mensagens básicas no console.
    
    ## Funcionalidade
    
    O programa realiza as seguintes ações:
    1. Exibe uma mensagem de boas-vindas.
    2. Exibe uma mensagem informando o nome do usuário.
    3. Exibe uma mensagem informando a idade do usuário.

---
+++ Welcome.java
@@ -0,0 +1,7 @@
+public class Welcome{
+    public static void main(String[] args){
+        System.out.println("Seja bem vindo");
+        System.out.println("Meu nome é Timóteo Dimande jr");
+        System.out.println("Tenho 18 anos de Idade");
+    }
+}
