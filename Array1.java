c7d0b961d7f2e4065847ef62b029e2ee800524c8
Timóteo Dimande júnior <timoteojrdimande@gmail.com>
2024-09-20 11:15:47 +0000

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,15 @@
+public class Array1{
+    public static void main(String[] args) {
+        
+        String[] cars = new String[3];
+
+        cars[0] = "Camaro";
+        cars[1] = "tesla";
+        cars[2] = "toyota";
+        
+        for (int i = 0; i < cars.length ; i++){
+            System.out.println(cars[i]);
+        }
+
+
+    }}
