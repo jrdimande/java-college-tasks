c76230ed3497283f2dba72773269dc4064a18a84
jrdimande <timoteojrdimande@gmail.com>
2025-04-26 12:58:47 +0000

    Class Objects and attributes

---
+++ second-year-tasks/OOP/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    int x = 5;
+
+    public static void main(String[] args){
+        Main myObj = new Main();
+        Main myObj1 = new Main();
+        System.out.println("Original: " + myObj.x);
+
+        // Modify attribute
+        myObj.x = 40;
+        System.out.println("Modified: " + myObj.x);
+
+        // Operations
+        System.out.println("Result: "myObj.x + myObj1.x);
+    }
+}
