8546eba6ccfb7d847d0b47de5d1cc6b6876faa8a
jrdimande <timoteojrdimande@gmail.com>
2025-04-26 18:22:39 +0000

    Class Portao

---
+++ second-year-tasks/Ficha3/exercise_3/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+        Portao p = new Portao();
+
+        // Testar os metodos
+        p.estaAberto();
+        p.abrir();
+        p.estaAberto();
+        p.fechar();
+        p.estaAberto();
+    }
+}
+
