636f2322f12516156033bc6a6a8c3183158b5f7c
jrdimande <timoteojrdimande@gmail.com>
2025-04-26 14:05:12 +0000

    Constructors and Encapsulation

---
+++ second-year-tasks/OOP/Encapsulation/example1/Main.java
@@ -0,0 +1,14 @@
+package example1;
+
+public class Main {
+    public static void main(String[] args){
+        Student s = new Student();
+        s.setName("Jay z");
+        s.setAge(40);
+        
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+
+
+    }
+}
