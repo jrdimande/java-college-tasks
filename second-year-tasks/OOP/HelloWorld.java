01ed551ed73af9500f2f8cb6fc941f4208d111c9
jrdimande <timoteojrdimande@gmail.com>
2025-04-30 19:33:31 +0000

    Resolvendo Exercicios

--- second-year-tasks/OOP/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    static void myMethod(){
-        System.out.println("Hello world!");
-    }
-
-    public static void main(String[] args){
-        myMethod();
-    }
-}
